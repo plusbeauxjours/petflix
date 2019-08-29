b72155044f3a0985a268effda7116c440facf104
plusbeauxjours <plusbeauxjours@gmail.com>
2019-08-29 22:23:22 +0900

    api

--- src/api.tsx
+++ src/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
     api_key: "01da2a336a604956c260900d1835847f",
@@ -8,4 +8,22 @@ const api = axios.create({
   }
 });
 
-export default api;
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movie: id =>
+    api.get(`movie/${id}`, { params: { append_to_response: "videos" } }),
+  search: term =>
+    api.get("search/move", { params: { query: encodeURIComponent(term) } })
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: id =>
+    api.get(`tv/${id}`, { params: { append_to_response: "videos" } }),
+  search: term =>
+    api.get("search/tv", { params: { query: encodeURIComponent(term) } })
+};
