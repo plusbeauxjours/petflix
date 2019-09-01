4930a04b16f1d5883d0269032b84aaafe99ac152
plusbeauxjours <plusbeauxjours@gmail.com>
2019-09-01 13:03:34 +0900

    Updates

---
+++ precache-manifest.ea373c515d401f2cedaea740de054bca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55a33cb65c9e0512aa57ac49fbf7e5f",
+    "url": "/petflix/index.html"
+  },
+  {
+    "revision": "b259e06ee4bc7553ed11",
+    "url": "/petflix/static/js/2.a740a014.chunk.js"
+  },
+  {
+    "revision": "45382fdad1cdf9532ff1",
+    "url": "/petflix/static/js/main.a1abe60e.chunk.js"
+  },
+  {
+    "revision": "17f017ec01718587c42a",
+    "url": "/petflix/static/js/runtime~main.c72d89c9.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/petflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
